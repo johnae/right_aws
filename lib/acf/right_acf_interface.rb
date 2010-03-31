9da9a9057d69c7237382da6133d1279a4918473b
Matt Hodgson <mhodgson@youcastr.com>
2010-03-31 16:58:11 -0400

    added support for cloud fron streaming and private streaming with origin access identities

--- lib/acf/right_acf_interface.rb
+++ lib/acf/right_acf_interface.rb
@@ -81,6 +81,7 @@ module RightAws
     DEFAULT_PORT     = 443
     DEFAULT_PROTOCOL = 'https'
     DEFAULT_PATH     = '/'
+    DISTRIBUTION_RESOURCE = {'http' => 'distribution', 'streaming' => 'streaming-distribution']
 
     @@bench = AwsBenchmarkingBlock.new
     def self.bench_xml
@@ -175,10 +176,19 @@ module RightAws
       hash
     end
 
-    def config_to_xml(config) # :nodoc:
+    def config_to_xml(config, type='http') # :nodoc:
       cnames = ''
+      config_type = type == 'http' ? 'DistributionConfig' : 'StreamingDistributionConfig'
       unless config[:cnames].blank?
         config[:cnames].to_a.each { |cname| cnames += "  <CNAME>#{cname}</CNAME>\n" }
+      end
+      access_identity = ''
+      unless config[:access_identity].blank?
+        access_identity_str += "\n          <OriginAccessIdentity>origin-access-identity/cloudfront/#{config[:access_identity].gsub('origin-access-identity/cloudfront/','')}</OriginAccessIdentity>"
+      end
+      trusted_signers = ''
+      if config[:trusted_signers]
+        trusted_signers_str += "\n          <TrustedSigners><Self/></TrustedSigners>"
       end
       # logging
       logging = ''
@@ -190,14 +200,25 @@ module RightAws
       end
       # xml
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-      "<DistributionConfig xmlns=\"http://#{@params[:server]}/doc/#{API_VERSION}/\">\n" +
+      "<#{config_type} xmlns=\"http://#{@params[:server]}/doc/#{API_VERSION}/\">\n" +
       "  <Origin>#{config[:origin]}</Origin>\n" +
       "  <CallerReference>#{config[:caller_reference]}</CallerReference>\n" +
       "  <Comment>#{AcfInterface::escape(config[:comment].to_s)}</Comment>\n" +
       "  <Enabled>#{config[:enabled]}</Enabled>\n" +
       cnames  +
+      access_identity +
+      trusted_signers +
       logging +
-      "</DistributionConfig>"
+      "</#{config_type}>"
+    end
+    
+    def identity_config_to_xml(config) # :nodoc:
+      # xml
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<CloudFrontOriginAccessIdentityConfig xmlns=\"http://#{@params[:server]}/doc/#{API_VERSION}/\">\n" +
+      "  <CallerReference>#{config[:caller_reference]}</CallerReference>\n" +
+      "  <Comment>#{AcfInterface::escape(config[:comment].to_s)}</Comment>\n" +
+      "</CloudFrontOriginAccessIdentityConfig>"
     end
 
     #-----------------------------------------------------------------
@@ -216,15 +237,32 @@ module RightAws
     #      :comment            => "My comments",
     #      :last_modified_time => Wed Sep 10 17:00:04 UTC 2008 }, ..., {...} ]
     #
-    def list_distributions
+    def list_distributions(type='http')
       result = []
-      incrementally_list_distributions do |response|
+      incrementally_list_distributions(:type => 'http') do |response|
         result += response[:distributions]
         true
       end
       result
     end
-
+    
+    # List all identities.
+    # Returns an array of identities or RightAws::AwsError exception.
+    #
+    #  acf.list_identities #=>
+    #    [{:aws_id                => "E4U91HCJHGXVC",
+    #      :s3_cononical_user_id  => "FIUDBVIUBSIUBSILUBSVIUBSVIUB2497y2439587",
+    #      :comment               => "My comments" }, ..., {...} ]
+    #
+    def list_identities
+      result = []
+      incrementally_list_identities do |response|
+        result += response[:identities]
+        true
+      end
+      result
+    end
+    
     # Incrementally list distributions.
     # 
     # Optional params: +:marker+ and +:max_items+.
@@ -255,20 +293,59 @@ module RightAws
     #     false # return false if the listing should be broken otherwise use true
     #   end
     #
-    def incrementally_list_distributions(params={}, &block)
+    def incrementally_list_distributions(params={:type => 'http'}, &block)
       opts = {}
       opts['MaxItems'] = params[:max_items] if params[:max_items]
       opts['Marker']   = params[:marker]    if params[:marker]
       last_response = nil
       loop do
-        link = generate_request('GET', 'distribution', opts)
+        link = generate_request('GET', DISTRIBUTION_RESOURCE[params[:type]], opts)
         last_response = request_info(link,  AcfDistributionListParser.new(:logger => @logger))
         opts['Marker'] = last_response[:next_marker]
         break unless block && block.call(last_response) && !last_response[:next_marker].blank?
       end 
       last_response 
     end
-
+    
+    
+    # Incrementally list identities.
+    # 
+    # Optional params: +:marker+ and +:max_items+.
+    #
+    #   # get first identity
+    #   incrementally_list_identities(:max_items => 1) #=>
+    #      {:distributions=>
+    #        [{:aws_id=>"E2Q0AOOMFNPSYL",
+    #          :s3_cononical_user_id  => "FIUDBVIUBSIUBSILUBSVIUBSVIUB2497y2439587",
+    #          :comment=>"ONE LINE OF COMMENT"}],
+    #       :is_truncated=>true,
+    #       :max_items=>1,
+    #       :marker=>"",
+    #       :next_marker=>"E2Q0AOOMFNPSYL"}
+    #
+    #   # get max 100 identities (the list will be restricted by a default MaxItems value ==100 )
+    #   incrementally_list_identities
+    #
+    #   # list identities by 10
+    #   incrementally_list_identities(:max_items => 10) do |response|
+    #     puts response.inspect # a list of 10 identities
+    #     false # return false if the listing should be broken otherwise use true
+    #   end
+    #
+    def incrementally_list_identities(params={}, &block)
+      opts = {}
+      opts['MaxItems'] = params[:max_items] if params[:max_items]
+      opts['Marker']   = params[:marker]    if params[:marker]
+      last_response = nil
+      loop do
+        link = generate_request('GET', 'origin-access-identity/cloudfront', opts)
+        last_response = request_info(link,  AcfOriginAccessIdentityListParser.new(:logger => @logger))
+        opts['Marker'] = last_response[:next_marker]
+        break unless block && block.call(last_response) && !last_response[:next_marker].blank?
+      end 
+      last_response 
+    end
+    
     # Create a new distribution.
     # Returns the just created distribution or RightAws::AwsError exception.
     #
@@ -287,21 +364,45 @@ module RightAws
     #     :last_modified_time => Wed Sep 10 17:00:54 UTC 2008,
     #     :caller_reference   => "200809102100536497863003"}
     #
-    def create_distribution(origin, comment='', enabled=true, cnames=[], caller_reference=nil, logging={})
+    def create_distribution(origin, comment='', enabled=true, cnames=[], caller_reference=nil, logging={}, access_identity=nil, trusted_signers=false, type='http')
       config = { :origin  => origin,
                  :comment => comment,
                  :enabled => enabled,
                  :cnames  => cnames.to_a,
-                 :caller_reference => caller_reference }
+                 :caller_reference => caller_reference,
+                 :access_identity => access_identity,
+                 :trusted_signers => trusted_signers }
       config[:logging] = logging unless logging.blank?
-      create_distribution_by_config(config)
+      create_distribution_by_config(config, type)
+    end
+
+    def create_distribution_by_config(config, type='http')
+      config[:caller_reference] ||= generate_call_reference
+      link = generate_request('POST', DISTRIBUTION_RESOURCE[type], {}, config_to_xml(config, type))
+      merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
+    end
+    
+    # Create a new identity.
+    # Returns the just created identity or RightAws::AwsError exception.
+    #
+    #  acf.create_identity('Woo-Hoo!' ) #=>
+    #    {:aws_id                 =>"E2Q0AOOMFNPSYL",
+    #     :s3_cononical_user_id   => "FIUDBVIUBSIUBSILUBSVIUBSVIUB2497y2439587",
+    #     :comment                => "Woo-Hoo!",
+    #     :caller_reference       => "200809102100536497863003"}
+    #
+    def create_identity(comment='', caller_reference=nil)
+      config = { :comment => comment,
+                 :caller_reference => caller_reference }
+      create_identity_by_config(config)
     end
 
     def create_distribution_by_config(config)
       config[:caller_reference] ||= generate_call_reference
-      link = generate_request('POST', 'distribution', {}, config_to_xml(config))
-      merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
+      link = generate_request('POST', 'origin-access-identity/cloudfront', {}, identity_config_to_xml(config))
+      merge_headers(request_info(link, AcfOriginAccessIdentityListParser.new(:logger => @logger))[:identities].first)
     end
+    
 
     # Get a distribution's information.
     # Returns a distribution's information or RightAws::AwsError exception.
@@ -318,9 +419,22 @@ module RightAws
     #     :origin             => "my-bucket.s3.amazonaws.com",
     #     :last_modified_time => Wed Sep 10 17:00:54 UTC 2008 }
     #
-    def get_distribution(aws_id)
-      link = generate_request('GET', "distribution/#{aws_id}")
+    def get_distribution(aws_id, type='http')
+      link = generate_request('GET', "#{DISTRIBUTION_RESOURCE[type]}/#{aws_id}")
       merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
+    end
+    
+    # Get an identity's information.
+    # Returns an identity's information or RightAws::AwsError exception.
+    #
+    #  acf.get_identity('E2REJM3VUN5RSI') #=>
+    #    {:aws_id                 => "E2REJM3VUN5RSI",
+    #     :s3_cononical_user_id   => "FIUDBVIUBSIUBSILUBSVIUBSVIUB2497y2439587",
+    #     :comment                => "Woo-Hoo!"}
+    #
+    def get_identity(aws_id)
+      link = generate_request('GET', "origin-access-identity/cloudfront/#{aws_id}")
+      merge_headers(request_info(link, AcfOriginAccessIdentityListParser.new(:logger => @logger))[:identities].first)
     end
 
     # Get a distribution's configuration.
@@ -334,10 +448,23 @@ module RightAws
     #     :comment          => "Woo-Hoo!",
     #     :origin           => "my-bucket.s3.amazonaws.com"}
     #
-    def get_distribution_config(aws_id)
-      link = generate_request('GET', "distribution/#{aws_id}/config")
+    def get_distribution_config(aws_id, type='http')
+      link = generate_request('GET', "#{DISTRIBUTION_RESOURCE[type]}/#{aws_id}/config")
       merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
     end
+    
+    # Get an identity's configuration.
+    # Returns an identity's configuration or RightAws::AwsError exception.
+    #
+    #  acf.get_identity_config('E2REJM3VUN5RSI') #=>
+    #    {:caller_reference => "200809102100536497863003",
+    #     :comment          => "Woo-Hoo!"}
+    #
+    def get_identity_config(aws_id)
+      link = generate_request('GET', "origin-access-identity/cloudfront/#{aws_id}/config")
+      merge_headers(request_info(link, AcfOriginAccessIdentityListParser.new(:logger => @logger))[:identities].first)
+    end
+    
 
     # Set a distribution's configuration 
     # (the :origin and the :caller_reference cannot be changed).
@@ -354,8 +481,23 @@ module RightAws
     #  config[:enabled] = false
     #  acf.set_distribution_config('E2REJM3VUN5RSI', config) #=> true
     #
-    def set_distribution_config(aws_id, config)
-      link = generate_request('PUT', "distribution/#{aws_id}/config", {}, config_to_xml(config),
+    def set_distribution_config(aws_id, config, type='http')
+      link = generate_request('PUT', "#{DISTRIBUTION_RESOURCE[type]}/#{aws_id}/config", {}, config_to_xml(config, type),
+                                     'If-Match' => config[:e_tag])
+      request_info(link, RightHttp2xxParser.new(:logger => @logger))
+    end
+    
+    # Set an identity's configuration 
+    # Returns +true+ on success or RightAws::AwsError exception.
+    #
+    #  config = acf.get_identity_config('E2REJM3VUN5RSI') #=>
+    #    {:caller_reference => "200809102100536497863003",
+    #     :comment          => "Woo-Hoo!"}
+    #  config[:comment] = 'Olah-lah!'
+    #  acf.set_identity_config('E2REJM3VUN5RSI', config) #=> true
+    #
+    def set_identity_config(aws_id, config)
+      link = generate_request('PUT', "origin-access-identity/cloudfront/#{aws_id}/config", {}, identity_config_to_xml(config),
                                      'If-Match' => config[:e_tag])
       request_info(link, RightHttp2xxParser.new(:logger => @logger))
     end
@@ -365,8 +507,19 @@ module RightAws
     #
     #  acf.delete_distribution('E2REJM3VUN5RSI', 'E39OHHU1ON65SI') #=> true
     #
-    def delete_distribution(aws_id, e_tag)
-      link = generate_request('DELETE', "distribution/#{aws_id}", {}, nil,
+    def delete_distribution(aws_id, e_tag, type='http')
+      link = generate_request('DELETE', "#{DISTRIBUTION_RESOURCE[type]}/#{aws_id}", {}, nil,
+                                        'If-Match' => e_tag)
+      request_info(link, RightHttp2xxParser.new(:logger => @logger))
+    end
+    
+    # Delete an identity. The enabled distribution cannot be deleted.
+    # Returns +true+ on success or RightAws::AwsError exception.
+    #
+    #  acf.delete_identity('E2REJM3VUN5RSI', 'E39OHHU1ON65SI') #=> true
+    #
+    def delete_identity(aws_id, e_tag)
+      link = generate_request('DELETE', "origin-access-identity/cloudfront/#{aws_id}", {}, nil,
                                         'If-Match' => e_tag)
       request_info(link, RightHttp2xxParser.new(:logger => @logger))
     end
@@ -380,9 +533,18 @@ module RightAws
         @result = { :distributions => [] }
       end
       def tagstart(name, attributes)
-        if name == 'DistributionSummary' || name == 'Distribution' ||
-          (name == 'DistributionConfig' && @xmlpath.blank?)
+        if name == 'DistributionSummary' || name == 'Distribution' || name == 'StreamingDistributionSummary' || name == 'StreamingDistribution' ||
+          (name == 'DistributionConfig' && @xmlpath.blank?) || (name == 'StreamingDistributionConfig' && @xmlpath.blank?)
           @distribution = { :cnames  => [], :logging => {} }
+        end
+        if name == 'TrustedSigners'
+          @current_signers = :trusted_signers
+          @distribution[@current_signers] = []
+        elsif name == 'ActiveTrustedSigners'
+          @current_signers = :active_trusted_signers
+          @distribution[@current_signers] = []
+        elsif name == 'Signer'
+          @distribution[@current_signers] << {}
         end
       end
       def tagend(name)
@@ -402,10 +564,55 @@ module RightAws
           when 'Enabled'          then @distribution[:enabled]            = @text == 'true' ? true : false
           when 'Bucket'           then @distribution[:logging][:bucket]   = @text
           when 'Prefix'           then @distribution[:logging][:prefix]   = @text
+          when 'Self'
+            if @current_signers == :active_trusted_signers
+              @distribution[:active_trusted_signers].last[:id] = 'self'
+              puts @distribution[@current_signers]
+            else
+              @distribution[@current_signers] << 'self'
+            end
+          when 'AwsAccountNumber'
+            if @current_signers == :active_trusted_signers
+              @distribution[@current_signers].last[:id] = @text
+              puts @distribution[@current_signers]
+            else
+              @distribution[@current_signers] << @text
+            end
+          when 'KeyPairId'
+            @distribution[@current_signers].last[:key_pairs] ||= []
+            @distribution[@current_signers].last[:key_pairs] << @text
         end
         if name == 'DistributionSummary' || name == 'Distribution' ||
           (name == 'DistributionConfig' && @xmlpath.blank?)
           @result[:distributions] << @distribution
+        end
+      end
+    end
+    
+    class AcfOriginAccessIdentityListParser < RightAWSParser # :nodoc:
+      def reset
+        @result = { :identities => [] }
+      end
+      def tagstart(name, attributes)
+        if name == 'CloudFrontOriginAccessIdentitySummary' || name == 'CloudFrontOriginAccessIdentity' ||
+          (name == 'CloudFrontOriginAccessIdentityConfig' && @xmlpath.blank?)
+          @identity = {}
+        end
+      end
+      def tagend(name)
+        case name
+          when 'Marker'      then @result[:marker]       = @text
+          when 'NextMarker'  then @result[:next_marker]  = @text
+          when 'MaxItems'    then @result[:max_items]    = @text.to_i
+          when 'IsTruncated' then @result[:is_truncated] = @text == 'true' ? true : false
+          when 'Id'                 then @identity[:aws_id]                 = @text
+          when 'S3CanonicalUserId'  then @identity[:s3_connonical_user_id]  = @text
+          when 'Comment'            then @identity[:comment]                = AcfInterface::unescape(@text)
+          when 'CallerReference'    then @identity[:caller_reference]       = @text
+        end
+        if name == 'CloudFrontOriginAccessIdentitySummary' || name == 'CloudFrontOriginAccessIdentity' ||
+          (name == 'CloudFrontOriginAccessIdentityConfig' && @xmlpath.blank?)
+          @result[:identities] << @identity
         end
       end
     end
